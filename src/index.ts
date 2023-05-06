42c80b0e7090867dd8b68f26de97ba1a2403eba9
rongmz <rounak.saha0507@gmail.com>
2023-05-07 00:13:40 +0530

    Change webpack build process. Now ts will be default. and webpack will only build for web umd bundle.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import * as TradingChart from './TradingChart';
-import * as types from './types';
-import * as d3 from 'd3';
-
-// ------------------------------------------------------------
-if (typeof (window) === 'object') {
-  if (!(window as any)['rongmz']) (window as any)['rongmz'] = {};
-  Object.assign((window as any)['rongmz'], {
-    ...TradingChart,
-    ...types,
-    d3: { ...d3 }
-  })
-}
+export * as TradingChart from './TradingChart';
+export * as types from './types';
+export * as d3 from 'd3';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 // const webpack = require('webpack');
 const path = require('path');
-const { readFileSync } = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'))
@@ -9,10 +9,27 @@ const name = packageJson.name;
 
 module.exports = env => {
   console.log('webpack building ', name, 'env=', env);
+  if (env.web) {
+    writeFileSync('./src/webpack-build.ts', `
+      import * as TradingChart from './TradingChart';
+      import * as types from './types';
+      import * as d3 from 'd3';
+
+      // ------------------------------------------------------------
+      if (typeof (window) === 'object') {
+        if (!(window as any)['rongmz']) (window as any)['rongmz'] = {};
+        Object.assign((window as any)['rongmz'], {
+          ...TradingChart,
+          ...types,
+          d3: { ...d3 }
+        })
+      }
+    `, 'utf-8');
+  }
 
   return {
     entry: {
-      [name]: './src/index.ts',
+      [name]: (env.web || env.WEBPACK_SERVE) ? './src/webpack-build.ts' : './src/index.ts',
       // [`${name}.min`]: './src/index.ts', // uncomment for prod
     },
     target: 'web',
