eb3581159127fc33d244dacf8250d2af0169f795
rongmz <rounak.saha0507@gmail.com>
2023-05-21 23:45:17 +0530

    More live update of settings Fixes #20

--- webpack.config.js
+++ webpack.config.js
@@ -11,20 +11,19 @@ module.exports = env => {
   console.log('webpack building ', name, 'env=', env);
   if (env.web) {
     writeFileSync('./src/webpack-build.ts', `
-      import * as TradingChart from './TradingChart';
-      import * as types from './types';
-      import * as d3 from 'd3';
+import * as TradingChart from './TradingChart';
+import * as types from './types';
+import * as d3 from 'd3';
 
-      // ------------------------------------------------------------
-      if (typeof (window) === 'object') {
-        if (!(window as any)['rongmz']) (window as any)['rongmz'] = {};
-        Object.assign((window as any)['rongmz'], {
-          ...TradingChart,
-          ...types,
-          d3: { ...d3 }
-        })
-      }
-    `, 'utf-8');
+// ------------------------------------------------------------
+if (typeof (window) === 'object') {
+  if (!(window as any)['rongmz']) (window as any)['rongmz'] = {};
+  Object.assign((window as any)['rongmz'], {
+    ...TradingChart,
+    ...types,
+    d3: { ...d3 }
+  })
+}`, 'utf-8');
   }
 
   return {
